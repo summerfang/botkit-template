c8f1bb6170fe2d650ab6352540ecc5e4c8c85c3f
Summer Fang <summerfang@gmail.com>
2022-03-31 22:48:32 -0700

    simple test

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -6,7 +6,7 @@
 module.exports = function(controller) {
 
     controller.hears('sample','message,direct_message', async (bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
+        await bot.reply(message, 'I heard ad d sample message.');
     });
 
     controller.on('message', async (bot, message) => {
