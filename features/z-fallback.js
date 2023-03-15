6cc0eb4ecbd8081a35bf1bba7949ee6dbd5b7bf0
Summer Fang <summerfang@gmail.com>
2023-03-15 01:20:45 -0700

    Support openapi

--- features/z-fallback.js
+++ features/z-fallback.js
@@ -7,7 +7,29 @@ module.exports = function (controller) {
     controller.on( 'message,direct_message', async ( bot, message ) => {
 
         let markDown = `Sorry, I did not understand.  \nTry: ${ controller.checkAddMention( message.roomType, 'help' ) }`;
-            
-        await bot.reply( message, { markdown: markDown } );
+        
+        try {
+            const response = await fetch("/api/generate", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ animal: 'Horse is faster' }),
+            });
+      
+            const data = await response.json();
+            if (response.status !== 200) {
+              throw data.error || new Error(`Request failed with status ${response.status}`);
+            }
+      
+            //setResult(data.result);
+            // setAnimalInput("");
+          } catch(error) {
+            // Consider implementing your own error handling logic here
+            console.error(error);
+            alert(error.message);
+          }
+
+        await bot.reply( message, { markdown: daat.data } );
     });
 }
